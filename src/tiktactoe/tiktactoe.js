1e3a2f2719c51666146218411254202b1f7178d1
chandiosamad07 <absamadchandio@gmail.com>
2023-05-09 23:43:17 +0500

    worked on events

--- src/tiktactoe/tiktactoe.js
+++ src/tiktactoe/tiktactoe.js
@@ -1,34 +1,98 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './tiktactoe.css'
 const Tictactoe = () =>{
-    const Cell = ()=>{
-        return <td> - </td>
+    const [turn, setTurn] = useState('x')
+    const [cells,setCells]=useState(Array(9).fill(''));
+    const [winner , serWinner] = useState();
+    const checkWinner = (squares) =>{
+        let combos = {
+          across:[
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            ],
+          down:[
+                [0, 3, 6],
+                [3, 4, 5],
+                [2, 5, 8],
+                ],
+      diagnol:[
+            [0, 3, 6],
+            [2, 4, 6],
+            
+            ]
+            
+        };
+
+        for(let combo in combos){
+            combos[combo].forEach((pattern) => {
+                if(squares[pattern[0]] === ''||
+                squares[pattern[1]] === ''||
+                squares[pattern[2]] === ''
+                    ){
+
+                    }
+                else if (
+                    squares[pattern[0]] === squares[pattern[1]] &&
+                    squares[pattern[1]] === squares[pattern[2]] 
+                ){
+                        serWinner( squares[pattern[0]] )
+                }
+            });
+        }
+    }
+
+    const handleClick = (num) =>{   
+        if(cells[num]!==''){
+            alert('already Clicked')
+        }
+        
+        let squares = [...cells];    
+        if(turn === 'x'){
+            squares[num]='x'
+            setTurn('o');
+        }
+        else{
+            squares[num]='o'
+            setTurn('x');
+        }
+        setCells(squares);
+    };
+    const Cell = ({num})=>{
+        return <td onClick={()=>handleClick(num)}>{cells[num]} - </td>
     }
     return (
         <div className = 'container'> 
            <table>
+            Turn:{turn}
             <tbody>
                 <tr>
-                    <Cell />
-                    <Cell />
-                    <Cell />
+                    <Cell num={0} />
+                    <Cell num={1} />
+                    <Cell num={2} />
                     
                 </tr>
                 <tr>
-                    <Cell />
-                    <Cell />
-                    <Cell />
+                    <Cell num={4} />
+                    <Cell num={5} />
+                    <Cell num={6} />
                     
                 </tr>
                 <tr>
-                    <Cell />
-                    <Cell />
-                    <Cell />
+                    <Cell num={7} />
+                    <Cell num={8} />
+                    <Cell num={9} />
                     
                 </tr>
             </tbody>
            </table>
+           {winner &&(
+            <>
+            <p>{winner} is the winner </p>
+            <button>Play Again</button>
+            </>
+           )}
         </div>
     );
     
